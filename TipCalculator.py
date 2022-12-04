ece4e3bae5a9a98ce4463e326cbd4d2394e259f2
RIRINJENJE <richellesmits.94@gmail.com>
2022-12-04 18:05:17 +0100

    added \n to clean up the code

--- TipCalculator.py
+++ TipCalculator.py
@@ -1,7 +1,7 @@
 # This is a tip calculator
 
 print("Welcome to the tip calculator.\n")
-bill = input("What was the total bill? $")
+bill = input("What was the total bill?\n $")
 percentage = input("\nWhat percentage tip would you like to give? 10, 12, or 15?\n")
 splitting = input("\nHow many people to split the bill?\n")
 
@@ -12,4 +12,4 @@ splitting_as_int = int(splitting)
 tip_costs = (bill_as_float / 100) * percentage_as_int
 final_bill = bill_as_float + tip_costs
 costs_per_person = round(float(final_bill) / float(splitting), 2)
-print(f"\nEach person should pay: ${costs_per_person}")
+print(f"\nEach person should pay:\n${costs_per_person}")

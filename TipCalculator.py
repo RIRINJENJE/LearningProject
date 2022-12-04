c791ae1f484238d42c7fbc6f9eb19c91893aab45
RIRINJENJE <richellesmits.94@gmail.com>
2022-12-04 18:24:47 +0100

    Simplified the code

--- TipCalculator.py
+++ TipCalculator.py
@@ -1,15 +1,11 @@
 # This is a tip calculator
 
 print("Welcome to the tip calculator.\n")
-bill = input("What was the total bill?\n $")
-percentage = input("\nWhat percentage tip would you like to give? 10, 12, or 15?\n")
-splitting = input("\nHow many people to split the bill?\n")
+bill = float(input("What was the total bill?\n $"))
+tip = int(input("\nWhat percentage tip would you like to give? 10, 12, or 15?\n"))
+people = int(input("\nHow many people to split the bill?\n"))
 
-bill_as_float = float(bill)
-percentage_as_int = int(percentage)
-splitting_as_int = int(splitting)
-
-tip_costs = (bill_as_float / 100) * percentage_as_int
-final_bill = bill_as_float + tip_costs
-costs_per_person = round(float(final_bill) / float(splitting), 2)
+tip_costs = (bill / 100) * tip
+final_bill = bill + tip_costs
+costs_per_person = round(float(final_bill) / float(people), 2)
 print(f"\nEach person should pay:\n${costs_per_person}")
